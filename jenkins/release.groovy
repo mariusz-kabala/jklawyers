1882142f0d89dd807e01e95c1b97e8669babd2c3
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-04-06 09:31:40 +0200

    ci: add release pipeline

---
+++ jenkins/release.groovy
@@ -0,0 +1,79 @@
+def SHORT_COMMIT;
+def branch = '';
+
+pipeline {
+    agent { docker { image 'docker-registry.kabala.tech/node12-with-git:latest' } }
+    environment {
+        app = ''
+        CI = 'true'
+        GIT_SSH_COMMAND = "ssh -o StrictHostKeyChecking=no"
+        GH_TOKEN = credentials('jenkins-github-accesstoken')
+    }
+
+    stages {
+        stage ('checkout') {
+            steps {
+                checkout([
+                        $class                           : 'GitSCM',
+                        branches                         : [[name: "${ghprbActualCommit}"]],
+                        browser                          : [$class: 'GithubWeb', repoUrl: "https://github.com/mariusz-kabala/jklawyers"],
+                        doGenerateSubmoduleConfigurations: false,
+                        userRemoteConfigs                : [[
+                                                                    credentialsId: 'github',
+                                                                    refspec      : '+refs/pull/*:refs/remotes/origin/pr/*',
+                                                                    url          : "git@github.com:mariusz-kabala/jklawyers.git"
+                                                            ]]
+                ])
+            }
+        }
+        stage ('prepare') {
+            steps {
+                script {
+                    sh "printenv"
+                    SHORT_COMMIT = GIT_COMMIT.take(7)
+                    env.SHORT_COMMIT = SHORT_COMMIT
+                    try {
+                        branch = env.GIT_LOCAL_BRANCH
+                        branch = branch ?: env.GIT_BRANCH
+                        if (branch == 'detached') {
+                            branch = ''
+                        }
+                        branch = branch ?: env.ghprbActualCommit
+                    } catch (e) {
+                        println "GIT BRANCH not detected"
+                    }
+
+                    sh 'git config user.name "jenkins-kabala.tech"'
+                    sh 'git config user.email "jenkins@kabala.tech"'
+
+                    if (!branch) {
+                        error "GIT branch to process not found"
+                    }
+
+                    if (branch.startsWith('origin/')) {
+                        branch = branch.replaceAll('origin/', '')
+                    }
+
+                    println "GIT branch to process: ${branch}"
+                    manager.addShortText(branch, "white", "navy", "1px", "navy")
+                }
+            }
+        }
+        stage ('Install dependencies') {
+            steps {
+                script {
+                    sh "npm i"
+                }
+            }
+        }
+        stage ('Release') {
+            steps {
+                sshagent(['jenkins-ssh-key']) {
+                    script {
+                        sh "npm run semantic-release -- -d"
+                    }
+                }
+            }
+        }
+    }
+}
