638f1ae2ba6bbf6f69d546b51116154e71963e2c
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-04-18 09:45:57 +0200

    feat: add google-analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,12 @@ module.exports = {
       },
     },
     "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-163892582-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
