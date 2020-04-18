da13f3e0739dbb985f8a46d09fb0d38e196e621b
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-04-18 11:51:43 +0200

    feat: working ui for contact form

--- src/components/contactForm/index.jsx
+++ src/components/contactForm/index.jsx
@@ -3,27 +3,44 @@ import { useForm } from 'react-hook-form'
 import styles from "./styles.module.scss"
 import { useTranslation } from 'react-i18next'
 import { Link } from "gatsby"
+import Recaptcha from 'react-recaptcha'
+import cn from 'classnames'
 
 export const ContactForm = () => {
     const { t, i18n } = useTranslation()
     const [isDisabled, setIsDisabled ] = useState(true)
+    const [isSuccess, setIsSuccess] = useState(false)
+    const { register, handleSubmit, errors } = useForm()
+    const onSubmit = data => { setIsSuccess(true) }
 
     return (
         <div className={styles.content}>
-            <form>
+            { isSuccess && (<div className={styles.success}>
+            <p>{t('email-sent-success-msg')} <br /><a onClick={() => setIsSuccess(false)}>{t('email-close')}</a></p>
+            </div>) }
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className={styles.element}>
-                    <label>
-                        <input type="text" name="name" placeholder={t('contact-name')} />
+                    <label className={cn({
+                        [styles.fieldError]: !!errors.name
+                    })}>
+                        <input type="text" name="name" placeholder={t('contact-name')} ref={register({ required: true })} />
+                        {errors.name && <p className={styles.error}><span>{t('field-is-required')}</span></p>}
                     </label>
                 </div>
                 <div className={styles.element}>
-                    <label>
-                        <input type="email" name="email" placeholder={t('contact-email')} />
+                    <label className={cn({
+                        [styles.fieldError]: !!errors.name
+                    })}>
+                        <input type="email" name="email" placeholder={t('contact-email')} ref={register({ required: true })} />
+                        {errors.email && <p className={styles.error}><span>{t('field-is-required')}</span></p>}
                     </label>
                 </div>
                 <div className={styles.element}>
-                    <label>
-                        <textarea name="message" placeholder={t('contact-message')} />
+                    <label className={cn({
+                        [styles.fieldError]: !!errors.name
+                    })}>
+                        <textarea name="message" placeholder={t('contact-message')} ref={register({ required: true })} />
+                        {errors.message && <p className={styles.error}><span>{t('field-is-required')}</span></p>}
                     </label>
                 </div>
                 <div className={styles.bottom}>
@@ -31,9 +48,18 @@ export const ContactForm = () => {
                         <input type="checkbox" onClick={() => setIsDisabled(!isDisabled)}/>
                         <Link to={`/${i18n.language}/information-clause`}>{t('consent')}</Link>
                     </div>
-                    <div className={styles.element}>
-                        <button disabled={isDisabled} type="submit">{t('contact-button')}</button>
+                    <div className={cn(styles.recaptcha, {
+                        [styles.isVisible]: !isDisabled
+                    })}>
+                    <Recaptcha
+                        sitekey="6LdoyuoUAAAAAJVS8YBMwv2cY4Cq5WPD7K9ZOr35"
+                        render="explicit"
+                        onloadCallback={() => null}
+                    />
                     </div>
+                </div>
+                <div className={styles.element}>
+                    <button disabled={isDisabled} type="submit">{t('contact-button')}</button>
                 </div>
             </form>
         </div>
