fb18aa260bdda55d85242f16462e8c1e6c26ad23
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-05-29 10:29:04 +0200

    feat: show langs as text only

--- src/components/languagesBar/index.jsx
+++ src/components/languagesBar/index.jsx
@@ -9,7 +9,7 @@ import cn from "classnames"
 import { globalHistory as history } from "@reach/router"
 import { navigate } from "gatsby"
 
-export const LanguagesBar = () => {
+export const LanguagesBar = ({showIcon = false}) => {
   const { i18n } = useTranslation()
   const { location } = history
   const getOnClickCallback = lang => () => {
@@ -29,37 +29,41 @@ export const LanguagesBar = () => {
       <li
         className={cn({
           [styles.active]: i18n.language === "pl",
+          [styles.text]: !showIcon,
         })}
       >
         <a onClick={getOnClickCallback("pl")}>
-          <img src={flagPL} alt="pl" />
+          {showIcon ? <img src={flagPL} alt="pl" /> : 'PL'}
         </a>
       </li>
       <li
         className={cn({
           [styles.active]: i18n.language === "en",
+          [styles.text]: !showIcon,
         })}
       >
         <a onClick={getOnClickCallback("en")}>
-          <img src={flagUK} alt="en" />
+          {showIcon ? <img src={flagUK} alt="en" /> : 'EN'}
         </a>
       </li>
       <li
         className={cn({
           [styles.active]: i18n.language === "de",
+          [styles.text]: !showIcon,
         })}
       >
         <a onClick={getOnClickCallback("de")}>
-          <img src={flagDE} alt="de" />
+          {showIcon ? <img src={flagDE} alt="de" /> : 'DE'}
         </a>
       </li>
       <li
         className={cn({
           [styles.active]: i18n.language === "fr",
+          [styles.text]: !showIcon,
         })}
       >
         <a onClick={getOnClickCallback("fr")}>
-          <img src={flagFR} alt="fr" />
+          {showIcon ? <img src={flagFR} alt="fr" /> : 'FR'}
         </a>
       </li>
     </ul>
